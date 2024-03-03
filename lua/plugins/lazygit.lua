240e9e719d002e9fd6a36e3e90e56c4f329ff692
momiji <anouluck.many@gmail.com>
2024-03-03 19:55:08 +0700

    add lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,18 @@
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = false,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
+    end
+}
